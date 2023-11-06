b344802c2dc64dc13036a013954c398bc4262637
Harshal Devmurari <92778686+Harshal2502@users.noreply.github.com>
2023-11-06 17:11:15 +0530

    writing unit tests

--- src/app/services/api.service.spec.ts
+++ src/app/services/api.service.spec.ts
@@ -1,18 +1,22 @@
 import { TestBed } from '@angular/core/testing';
 import { ApiService } from './api.service';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import axios from 'axios';
+import * as MockAdapter from 'axios-mock-adapter';
 
 describe('ApiService', () => {
   let apiService: ApiService;
   let httpTestingController: HttpTestingController;
+  let axiosMock: MockAdapter;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
     });
-    
+
     apiService = TestBed.inject(ApiService);
     httpTestingController = TestBed.inject(HttpTestingController);
+    axiosMock = new MockAdapter(axios);
   });
 
   it('should be created', () => {
@@ -26,5 +30,15 @@ describe('ApiService', () => {
     apiService.getUser(username).subscribe();
     const req = httpTestingController.expectOne(expectedUrl);
     expect(req.request.method).toEqual('GET');
+  });
+
+  it('should call the getRepositories method with the correct URL', async () => {
+    const url = 'https://api.github.com/users/testuser/repos';
+    const responseData = [{ name: 'repo1' }, { name: 'repo2' }];
+
+    axiosMock.onGet(url).reply(200, responseData);
+
+    const result = await apiService.getRepositories(url);
+    expect(result.data).toEqual(responseData);
   });
 });
